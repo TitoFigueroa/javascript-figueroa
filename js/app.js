7c5602c57a971eaafde898453b4c804b090607fc
JorgeFigueroa <jorgefigueroavindel@gmail.com>
2023-04-05 23:06:58 -0300

    Update logic to use now, arrays, superior order functions, more loops tools, objects declaration, a literal one for now, not a constructed one, with entire new code, now simulating an ecommerce

--- js/app.js
+++ js/app.js
@@ -1,32 +1,58 @@
-//Calculadora relacion de cilindrada total
-// La formula de calculo de cilindrada en centimetros cubicos es ((((diametro*diametro*pi)/4)*carrera)*cantidadCilindros)/1000
+//Aplicacion e-commerce de elementos para autos
+let startMessage = prompt("Para comenzar con su compra, por favor indicanos tu nombre: ")
+let fisrtBuy = true
+const reproTypesPrices = [{name:"stage1",precio:9000},{name:"stage2",precio:12000},{name:"stage3",precio:15000}] 
 
-
-let diametroCilindro = prompt("Ingrese diametro de cilindro en milimetros (ejemplo 86.4): ");
-let desplazamientoPiston = prompt("Ingrese el desplazamiento del piston en milimetros (ejemplo 67.4): ");
-let cantidadCilindros = prompt("Ingrese la cantidad de cilindros de su motor (ejemplo 4): ");
-const piNumber = 3.1416;
-const constanteCalc = 4;
-let calc = true;
-
-while (calc == true){
-    if ((diametroCilindro != "") && (desplazamientoPiston != "") && (cantidadCilindros != "")  ){
-        Number(diametroCilindro)
-        Number(desplazamientoPiston)
-        Number(cantidadCilindros)
-        cilindradaTotalCalc = ((((diametroCilindro*diametroCilindro*piNumber)/constanteCalc)*desplazamientoPiston)*cantidadCilindros)/1000
-        alert("La cilindrada de su motor es: " + parseInt(cilindradaTotalCalc) + " cc3")
+function showReproInfo(reproType){
+    switch (reproType) {
+        case "1":
+            return Object.values(reproTypesPrices[reproType-1])
+            break;
+        case "2":
+            return Object.values(reproTypesPrices[reproType-1])
+            break;
+        case "3":
+            return Object.values(reproTypesPrices[reproType-1])
+            break;
+    
+        default:
+            break;
     }
-    else{
-        alert("Por favor complete los campos, al parecer uno o mas valores no han sido ingresados")
-        break
+}
+
+function main(name){
+    while (fisrtBuy === true) {
+    let todayDate = new Date()
+    welcomeMessageInput = prompt ("Hola " + name + " , te damos la bienvenida al sitio donde encontraras los mejores chips para tu auto \n"
+    + "Elegi por favor que tipo de stage potencia desea y le compartiremos el costo del mismo: \n"
+    + "\n 1)stage1 \n 2)stage2 \n 3)stage3:")
+    if (welcomeMessageInput == 1  || welcomeMessageInput == 2 || welcomeMessageInput == 3){
+        if (welcomeMessageInput == 1){
+            alert("El precio al dia de la fecha " + todayDate.toLocaleDateString() + " del " + showReproInfo(welcomeMessageInput)[0] + " es: " + showReproInfo(welcomeMessageInput)[1])
+            fisrtBuy = false
+        }
+        else if (welcomeMessageInput == 2){
+            alert("El precio al dia de la fecha " + todayDate.toLocaleDateString() + " del " + showReproInfo(welcomeMessageInput)[0] + " es: " + showReproInfo(welcomeMessageInput)[1])
+            fisrtBuy = false
+        }
+        else if (welcomeMessageInput == 3){
+            alert("El precio al dia de la fecha " + todayDate.toLocaleDateString() + " del " + showReproInfo(welcomeMessageInput)[0] + " es: " + showReproInfo(welcomeMessageInput)[1])
+            fisrtBuy = false
+        }
     }
-    seguirCalculando = prompt("Desea calcular otra cilindrada?, escriba SI para continuar, enter para finalizar:")
-    if ((seguirCalculando == "SI") || (seguirCalculando == "si") ){
-        calc = false
+        else{
+            alert("Ud ha ingresado un valor incorrecto, por favor ingrese 1 para seleccionar stage1,  2 para stage2 o 3 para stage3")
+            location.reload()
+        } 
+    }
+    
+    secondOffer = prompt("Desea comprar metanol para potenciar aun mas su auto?, escriba SI. En caso negativo, NO para finalizar la compra ")
+    if(secondOffer == "si" || secondOffer == "SI"){
         location.reload()
     }
     else{
-        break
+        alert("Muchas gracias por su compra, que disfrutes tu vehiculo potenciado. Hasta la proxima")
     }
 }
+
+main(startMessage)
