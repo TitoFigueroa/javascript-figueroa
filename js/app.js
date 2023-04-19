d92c727e5b43c01454aedd6febf5c9fd97968be5
JorgeFigueroa <jorgefigueroavindel@gmail.com>
2023-04-19 18:46:52 -0300

    creating a new proyect, now its an e-commerce simulated platform, handly the DOM, using events, and localStorage as JSON, no more prompts or alerts :)

--- js/app.js
+++ js/app.js
@@ -1,58 +1,90 @@
-//Aplicacion e-commerce de elementos para autos
-let startMessage = prompt("Para comenzar con su compra, por favor indicanos tu nombre: ")
-let fisrtBuy = true
-const reproTypesPrices = [{name:"stage1",precio:9000},{name:"stage2",precio:12000},{name:"stage3",precio:15000}] 
+// Define the products data
+const products = [
+	{ id: 1, name: 'Product 1', price: 10.00 },
+	{ id: 2, name: 'Product 2', price: 20.00 },
+	{ id: 3, name: 'Product 3', price: 30.00 },
+	{ id: 4, name: 'Product 4', price: 40.00 },
+];
 
-function showReproInfo(reproType){
-    switch (reproType) {
-        case "1":
-            return Object.values(reproTypesPrices[reproType-1])
-            break;
-        case "2":
-            return Object.values(reproTypesPrices[reproType-1])
-            break;
-        case "3":
-            return Object.values(reproTypesPrices[reproType-1])
-            break;
-    
-        default:
-            break;
+// Retrieve the products from local storage, or use the default data if no products are stored
+let cart = localStorage.getItem('cart');
+if (!cart) {
+	cart = [];
+	localStorage.setItem('cart', JSON.stringify(cart));
+} else {
+	cart = JSON.parse(cart);
+}
+
+// Display the products
+function displayProducts() {
+	const productsDiv = document.getElementById('products');
+	productsDiv.innerHTML = '';
+	products.forEach(product => {
+		const productDiv = document.createElement('div');
+		productDiv.innerHTML = `
+			<h2>${product.name} - $${product.price}</h2>
+			<button data-id="${product.id}">Add to cart</button>
+		`;
+		const button = productDiv.querySelector('button');
+		button.addEventListener('click', addToCart);
+		productsDiv.appendChild(productDiv);
+	});
+}
+
+// Add a product to the cart
+function addToCart(event) {
+	const productId = parseInt(event.target.getAttribute('data-id'));
+	const product = products.find(p => p.id === productId);
+	if (product) {
+		// Check if the product is already in the cart
+		const existingProduct = cart.find(p => p.id === productId);
+		if (existingProduct) {
+			// If it is, increase the quantity
+			existingProduct.quantity++;
+		} else {
+			// Otherwise, add it to the cart with a quantity of 1
+			cart.push({ ...product, quantity: 1 });
+		}
+		localStorage.setItem('cart', JSON.stringify(cart));
+		displayCart();
+	}
+}
+
+// Display the shopping cart
+function displayCart() {
+    const cartDiv = document.getElementById('cart');
+    cartDiv.innerHTML = '';
+
+    let totalPrice = 0;
+    if (cart.length > 0) {
+        const cartList = document.createElement('ul');
+        cart.forEach(product => {
+            const cartItem = document.createElement('li');
+            cartItem.innerText = `${product.name} - $${product.price} x ${product.quantity} = $${product.price * product.quantity}`;
+            cartList.appendChild(cartItem);
+            totalPrice += product.price * product.quantity;
+        });
+        cartDiv.appendChild(cartList);
+
+        // Add a checkout button and display the total price
+        const checkoutButton = document.createElement('button');
+        checkoutButton.innerText = `Checkout - Total: $${totalPrice.toFixed(2)}`;
+        checkoutButton.addEventListener('click', checkout);
+        cartDiv.appendChild(checkoutButton);
+    } else {
+        cartDiv.innerHTML = '<p>Your cart is empty.</p>';
     }
 }
 
-function main(name){
-    while (fisrtBuy === true) {
-    let todayDate = new Date()
-    welcomeMessageInput = prompt ("Hola " + name + " , te damos la bienvenida al sitio donde encontraras los mejores chips para tu auto \n"
-    + "Elegi por favor que tipo de stage potencia desea y le compartiremos el costo del mismo: \n"
-    + "\n 1)stage1 \n 2)stage2 \n 3)stage3:")
-    if (welcomeMessageInput == 1  || welcomeMessageInput == 2 || welcomeMessageInput == 3){
-        if (welcomeMessageInput == 1){
-            alert("El precio al dia de la fecha " + todayDate.toLocaleDateString() + " del " + showReproInfo(welcomeMessageInput)[0] + " es: " + showReproInfo(welcomeMessageInput)[1])
-            fisrtBuy = false
-        }
-        else if (welcomeMessageInput == 2){
-            alert("El precio al dia de la fecha " + todayDate.toLocaleDateString() + " del " + showReproInfo(welcomeMessageInput)[0] + " es: " + showReproInfo(welcomeMessageInput)[1])
-            fisrtBuy = false
-        }
-        else if (welcomeMessageInput == 3){
-            alert("El precio al dia de la fecha " + todayDate.toLocaleDateString() + " del " + showReproInfo(welcomeMessageInput)[0] + " es: " + showReproInfo(welcomeMessageInput)[1])
-            fisrtBuy = false
-        }
-    }
-        else{
-            alert("Ud ha ingresado un valor incorrecto, por favor ingrese 1 para seleccionar stage1,  2 para stage2 o 3 para stage3")
-            location.reload()
-        } 
-    }
-    
-    secondOffer = prompt("Desea comprar metanol para potenciar aun mas su auto?, escriba SI. En caso negativo, NO para finalizar la compra ")
-    if(secondOffer == "si" || secondOffer == "SI"){
-        location.reload()
-    }
-    else{
-        alert("Muchas gracias por su compra, que disfrutes tu vehiculo potenciado. Hasta la proxima")
-    }
+// Checkout the items in the cart
+function checkout() {
+    const totalPrice = cart.reduce((total, product) => total + product.price * product.quantity, 0);
+    alert(`Total price: $${totalPrice.toFixed(2)}`);
+    cart = [];
+    localStorage.setItem('cart', JSON.stringify(cart));
+    displayCart();
 }
 
-main(startMessage)
+// Display the products and cart
+displayProducts();
+displayCart();
